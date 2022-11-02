352f9c2afd69f2517d1c87b1a1b0a8248cb733e3
amirrezaask <amirreza.askarpour@snapp.cab>
2022-11-02 11:34:38 +0330

    repogen

---
+++ cmd/repogen/example.go
@@ -0,0 +1,14 @@
+package main
+
+
+// repogen: model user
+type User struct {}
+
+
+// repogen: repo user
+type UserRepository interface{
+    FindByID()
+    FindByUserNameAndID()
+    UpdateUserName()
+    
+} 
